e19345e8d5a8b179bdb8572dc232a489e0c7541a
Holy0S <trevorjamessmith2005@gmail.com>
2025-12-05 13:25:23 -0800

    Project Preview Hover CSS

--- src/components/ProjectPreview.jsx
+++ src/components/ProjectPreview.jsx
@@ -4,15 +4,17 @@ function ProjectPreview( {title, description, image, link} ) {
     console.log(image)
     return(
         <a href={link}>
-        <div className="project-preview-container">
-            <img className="project-preview-image" src={image} alt={title}/>
-            <div className="project-preview-title">
-                {title}
+            <div className="project-card-container">
+                <div className="project-preview-card">
+                    <img className="project-preview-image" src={image} alt={title}/>
+                    <div className="project-preview-title">
+                        {title}
+                    </div>
+                    <div className="project-preview-description">
+                        {description}
+                    </div>
+                </div>
             </div>
-            <div className="project-preview-description">
-                {description}
-            </div>
-        </div>
         </a>
     )
 }
