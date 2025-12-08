ac8c3db09ce5645609b16df9a0079aece0d5bb71
Holy0S <trevorjamessmith2005@gmail.com>
2025-12-08 14:54:35 -0800

    Recent Projects on Home Page

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -3,9 +3,11 @@ import "../css/Background.css";
 function Background() {
     return (
         <div className="background">
-            <div className="square-1"/>
-            <div className="square-2"/>
-            <div className="square-3"/>
+            {
+            // <div className="square-1"/>
+            // <div className="square-2"/>
+            // <div className="square-3"/>
+            }
         </div>
     )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,7 @@
 import Header from '../components/Header.jsx';
 import Background from '../components/Background.jsx';
+import ProjectPreview from '../components/ProjectPreview.jsx'
+import "../css/HomePage.css";
 import '../css/App.css';
 
 function HomePage() {
@@ -8,10 +10,14 @@ function HomePage() {
             <Header/>
             <div className="page-container">
                 <div className="page">
-                <h1>Home</h1>
-                <h2>Hi- I'm Trevor</h2>
-                <h2>I'm a 3rd year computer science student at Oregon State University.</h2>
-
+                    <h1>Hi- I'm Trevor</h1>
+                    <h2>I'm a 3rd year computer science student at Oregon State University.</h2>
+                    <h2>Latest Work</h2>
+                    <div className="projects-container">
+                        <ProjectPreview></ProjectPreview>
+                        <ProjectPreview></ProjectPreview>
+                        <ProjectPreview></ProjectPreview>
+                    </div>
                 </div>
                 <Background/>
             </div>
