34cc063caf6df6340672809153550a064a147774
Holy0S <trevorjamessmith2005@gmail.com>
2025-12-20 15:32:26 -0800

    Added Headshot to Homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -12,12 +12,7 @@ function HomePage() {
                 <div className="page">
                     <h1>Hi- I'm Trevor</h1>
                     <h2>I'm a 3rd year computer science student at Oregon State University.</h2>
-                    <h2>Latest Work</h2>
-                    <div className="projects-container">
-                        <ProjectPreview image="cat2.jpg" title="Recent Project 2" description="Recent Project 2"></ProjectPreview>
-                        <ProjectPreview image="cat1.jpg" title="Recent Project 1" description="Recent Project 1"></ProjectPreview>
-                        <ProjectPreview image="cat3.jpg" title="Recent Project 3" description="Recent Project 3"></ProjectPreview>
-                    </div>
+                    <img className="headshot" src="/headshot.jpg"/>
                 </div>
                 <Background/>
             </div>
